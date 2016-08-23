4e0f81ec0e8343db38c8279c3ea12e25a56ef282
Peter Beard <peter.b.beard+github@gmail.com>
2016-08-23 03:25:21 -0400

    Add install script

---
+++ install.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# Copy everything in this directory to its home in ~
+read -p "This script will overwrite files. Are you sure you want to continue? [y/N] " CONT
+if [[ "$CONT" != "y" && "$CONT" != "Y" ]]; then
+    exit 1
+fi
+
+# Directories
+find * -maxdepth 1 -type d -print | while read f; do
+    rsync -vr "$f/" "$HOME/.${f#.}"
+done
+
+# Files
+find * -maxdepth 1 -type f -not -name "*.sh" -print | while read f; do
+    rsync -v "$f" "$HOME/.${f#.}"
+done
